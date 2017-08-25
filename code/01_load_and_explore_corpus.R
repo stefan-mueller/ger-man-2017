d55d428b49376e0132862e9832d62d00f440bfa3
Stefan Müller <mullers@tcd.ie>
2017-08-25 21:58:45 +0100

    Add additional manifestos

--- code/01_load_and_explore_corpus.R
+++ code/01_load_and_explore_corpus.R
@@ -12,17 +12,24 @@ library(dplyr)
 ## Load corpus (corpus_ger_man_2017)
 load("manifestos-corpus/corpus_ger_man_2017.RData")
 
+## Only select six major parties
+
+parties_select <- c("CDU-CSU", "SPD", "AfD", "Gruene", "Linke", "FDP")
+
+corpus_ger_man_2017 <- corpus_subset(corpus_ger_man_2017, party %in% parties_select)
+
 # Check occurence of word "Gerechtigkeit" (justice)
 textplot_xray(kwic(corpus_ger_man_2017, "Gerechtigkeit"))
 ggsave("output/plot_xray.png", width = 8, height = 4)
+
 
 ## Define additonal German stopwords
 stopwords_additional <- c("ab", "dass", "deshalb", "seit",
                           "statt", "n", "sowie")
 
-## Transform to documement feature matrix
+## Transform to document feature matrix
 dfm_man <- dfm(corpus_ger_man_2017, remove = c(stopwords("german"), stopwords_additional),
-               tolower = TRUE, remove_punct = TRUE, remove_numbers = TRUE)
+               tolower = TRUE, remove_punct = TRUE, remove_numbers = TRUE, remove_url = TRUE)
 
 ## Weight dfm by relative frequency
 dfm_man_weight <- dfm_weight(dfm_man, type = "relfreq")
@@ -39,11 +46,11 @@ freq_ordered$order <- 1:nrow(freq_ordered)
 ## Plot most frequent words by party
 ggplot(data = freq_ordered, aes(x = order, y = frequency)) +
   geom_point() +
-  facet_wrap(~ group, scales = "free", nrow = 2) +
+  facet_wrap(~ group, scales = "free") +
   coord_flip() +
   scale_x_continuous(breaks = freq_ordered$order, 
                      labels = freq_ordered$feature) +
-  labs(x = NULL, y = "Relative frequency") + 
+  labs(x = NULL, y = "Relative word frequency") + 
   theme_custom()
 ggsave("output/plot_topfeatures.pdf", width = 12, height = 6)
 ggsave("output/plot_topfeatures.png", width = 12, height = 6)
@@ -53,7 +60,7 @@ ggsave("output/plot_topfeatures.png", width = 12, height = 6)
 dfm_man_trim <- dfm_trim(dfm_man, min_count = 2)
 
 ## Run Wordfish model
-model_wordfish <- textmodel_wordfish(dfm_man_trim, dir = c(1, 2))
+model_wordfish <- textmodel_wordfish(dfm_man)
 
 ## Run correspondence analysis
 model_ca <- textmodel_ca(dfm_man_trim)
