38ce5b2bf4430f6bca5c76eb95dbda6eaf90a81f
Stefan Müller <mullers@tcd.ie>
2017-08-28 23:04:42 +0100

    Distinguish between election manifestos and general party programmes

--- code/01_read_manifestos.R
+++ code/01_read_manifestos.R
@@ -5,6 +5,7 @@
 ## Load required packages
 library(quanteda)
 library(readtext)
+library(dplyr)
 
 ## Load custom ggplot2 scheme
 source("code/theme_custom.R")
@@ -13,7 +14,7 @@ source("code/theme_custom.R")
 texts_man <- readtext("manifestos-pdf/*.pdf", 
                       docvarsfrom = "filenames",
                       dvsep = "_",
-                      docvarnames = c("year", "party"))
+                      docvarnames = c("year", "party", "type"))
 
 texts_man_data_frame <- as.data.frame(texts_man) %>% 
   select(-doc_id)
@@ -24,25 +25,22 @@ write.csv(texts_man_data_frame, "manifestos-corpus/ger_man_2017.csv",
 ## Transform to quantda corpus
 corpus_ger_man_2017 <- corpus(texts_man,
                               metacorpus = list(notes = "Corpus created by Stefan Müller (muellerstefan.net)",
-                                         source = "Party manifestos of 2017 German federal election (https://bundestagswahl-2017.com/wahlprogramm/)"))
+                                         source = "Party manifestos of 2017 German federal election"))
 
 docnames(corpus_ger_man_2017) <- docvars(corpus_ger_man_2017, "party")
 
-summary(corpus_ger_man_2017)
+summary(corpus_ger_man_2017, 3)
 
 
-# Corpus consisting of 6 documents.
+# Corpus consisting of 24 documents, showing 3 documents.
 # 
-# Text Types Tokens Sentences year  party
-# AfD  5972  21272       742 2017    AfD
-# CDU  4974  23034      1291 2017    CDU
-# FDP  9039  43383      2039 2017    FDP
-# Gruene 13185  81525      4011 2017 Gruene
-# Linke 12308  77004      2885 2017  Linke
-# SPD  8396  43827      2411 2017    SPD
+# Text Types Tokens Sentences year        party      type
+# AfD  5972  21272       742 2017          AfD manifesto
+# BUESO   718   1497        55 2017        BUESO programme
+# Bayernpartei  4646  17605       846 2017 Bayernpartei programme
 # 
-# Source:  Party manifestos of 2017 German federal election (https://bundestagswahl-2017.com/wahlprogramm/)
-# Created: Fri Aug 18 16:50:40 2017
+# Source:  Party manifestos of 2017 German federal election
+# Created: Mon Aug 28 23:03:20 2017
 # Notes:   Corpus created by Stefan Müller (muellerstefan.net)
 
 ## Save corpus as Rdata file
